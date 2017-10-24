58aaac7fb2045efca146d76c3ed40a5f92045ffe
YeicobXD <jacobmaldonado99@gmail.com>
2017-10-23 20:19:03 -0500

    lists added

--- src/colas/Lista.java
+++ src/colas/Lista.java
@@ -1,24 +1,25 @@
 package colas;
 
 public class Lista {
+
     private int contador;
     protected Nodo primero;
+    protected Nodo ultimo;
 
     public Lista() {
         contador = 0;
         primero = null;
+        ultimo = null;
     }
 
     public Lista insertarCabezaLista(String entrada) {
-        Nodo nuevo;
-        nuevo = new Nodo(entrada);
-        nuevo.enlace = primero;
-        primero = nuevo;
+        ultimo.enlace = primero;
+        primero = ultimo;
         contador++;
         return this;
     }
-    
-    public int cantidad(){
+
+    public int cantidad() {
         return contador;
     }
 
@@ -28,6 +29,71 @@ public class Lista {
         while (n != null) {
             System.out.print(n.dato + "\n");
             n = n.enlace;
+        }
+    }
+
+    public Lista insertarUltimo( String entrada) {
+        
+        ultimo.enlace = new Nodo(entrada);
+        ultimo = ultimo.enlace;
+        return this;
+    }
+
+    public Lista insertarLista(String antiguo, String entrada) {
+        Nodo anterior;
+        anterior = buscarLista(antiguo);
+        Nodo nuevo;
+        nuevo = new Nodo(entrada);
+        nuevo.enlace = anterior.enlace;
+        anterior.enlace = nuevo;
+        return this;
+    }
+    
+
+    public Nodo buscarLista(String destino) {
+        Nodo indice;
+        for (indice = primero; indice != null; indice = indice.enlace) {
+            if (destino.equals(indice.dato))
+            {
+                return indice;
+            }
+        }
+        return null;
+    }
+    
+    public void desplegar(){
+        Nodo indice;
+        for (indice = primero; indice != null; indice = indice.enlace) {
+            System.out.println(indice.dato);
+        }
+    }
+
+    public void eliminar(String entrada) {
+        Nodo actual, anterior;
+        boolean encontrado;
+//inicializa los apuntadores
+        actual = primero;
+        anterior = null;
+        encontrado = false;
+// búsqueda del nodo y del anterior
+        while ((actual != null) && (!encontrado)) {
+            encontrado = (actual.dato == entrada);
+//con objetos: actual.dato.equals(entrada)
+            if (!encontrado) {
+                anterior = actual;
+                actual = actual.enlace;
+            }
+        }
+// Enlace del nodo anterior con el siguiente
+        if (actual != null) {
+// Distingue entre que el nodo sea el cabecera,
+// o del resto de la lista
+            if (actual == primero) {
+                primero = actual.enlace;
+            } else {
+                anterior.enlace = actual.enlace;
+            }
+            actual = null; // no es necesario al ser una variable local
         }
     }
 }
---
+++ src/colas/MenuListasEnlazadas.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package colas;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author jacob
+ */
+public class MenuListasEnlazadas {
+    private static int contador =0;
+
+    public static void main(String[] args) {
+        Lista lista = new Lista();
+        
+        int opciones;
+        
+        do{
+            opciones = Integer.parseInt(JOptionPane.showInputDialog(""
+                    + "      MENU     \n "
+                    + "1- Insertar al principio.\n"
+                    + "2- Insertar al final.\n"
+                    + "3- Insertar en una posición especifica.\n"
+                    + "4.- Eliminar.\n "
+                    + "5.- Desplegar\n"
+                    + "6- Salir\n"));
+            try{
+            options(opciones,lista);
+            }catch(Exception e){
+                System.out.println("Error");
+            }
+        }while(opciones !=6 && opciones>=1 && opciones <=6);
+        
+    }
+    
+    public static void options(int n, Lista lista) throws Exception{
+        switch(n){
+            case 1:
+                String palabra;
+                palabra = JOptionPane.showInputDialog("Inserta el valor: ");
+                JOptionPane.showMessageDialog(null, "Has ingresado el valor: " + palabra);
+                lista.insertarCabezaLista(palabra);
+                contador ++;
+                break;
+            case 2:
+                palabra = JOptionPane.showInputDialog("Inserta el valor: ");
+                
+                JOptionPane.showMessageDialog(null, "Has ingresado el valor: " + palabra);
+                lista.insertarUltimo(palabra);
+               
+                contador --;
+                break;
+            case 3:
+                String antiguo;
+                antiguo = JOptionPane.showInputDialog("Inserta el valor: ");
+                
+                palabra = JOptionPane.showInputDialog("Inserta el valor: ");
+                
+                JOptionPane.showMessageDialog(null, "Has ingresado el valor: " + palabra);
+                lista.insertarLista(antiguo, palabra);
+               
+                
+                break;
+            case 4:
+                palabra = JOptionPane.showInputDialog("Inserta el valor a eliminar: ");
+                
+                JOptionPane.showMessageDialog(null, "Has eliminado el valor: " + palabra);
+                lista.eliminar(palabra);
+                break;
+                
+            case 5:
+                lista.desplegar();
+                
+            default:
+        }
+    }
+}
